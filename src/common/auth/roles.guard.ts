86241698488779fc9f5e0e66fde0546876170ff3
Gautam Kumar <gautam01kr11@gmail.com>
2025-12-26 15:10:46 +0530

    role based issue resolved

--- src/common/auth/roles.guard.ts
+++ src/common/auth/roles.guard.ts
@@ -1,18 +1,24 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { ExecutionContext, Injectable } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
+import { AuthGuard } from '@nestjs/passport';
 import { ROLES_KEY } from './roles.decorator';
 
 @Injectable()
-export class RolesGuard implements CanActivate {
-  constructor(private readonly reflector: Reflector) {}
+export class RolesGuard extends AuthGuard('jwt') {
+  constructor(private readonly reflector: Reflector) {
+    super();
+  }
 
-  canActivate(context: ExecutionContext): boolean {
+  async canActivate(context: ExecutionContext): Promise<boolean> {
     const requiredRoles = this.reflector.getAllAndOverride<string[]>(ROLES_KEY, [
       context.getHandler(),
       context.getClass(),
     ]);
 
     if (!requiredRoles || requiredRoles.length === 0) return true;
+
+    const can = (await super.canActivate(context)) as boolean;
+    if (!can) return false;
 
     const request = context.switchToHttp().getRequest();
     const user = request.user as { role?: string } | undefined;
